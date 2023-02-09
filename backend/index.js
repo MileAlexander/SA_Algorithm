77ce54d038d4f0b6c985802b525da8b94d5f429d
Mile Alexander <milealexander@gmail.com>
2023-02-09 20:02:00 +0100

    fixups

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,27 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const fs = require('fs')
-const stringify = require('csv-stringify').stringify
 const parse = require('csv-parse').parse
-const os = require('os')
-const multer  = require('multer')
-const upload = multer({ dest: 'CSV' })
 const port = 3000
 
-app.use(bodyParser.json())
 app.use(express.static('../frontend'))
+app.use(express.json())
+
 
 app.get('/csv', (req, res) => {
-    fs.readFile('CSV/school_book.csv', 'utf8', function (err,data) {
-        res.send(data);
+    const arr = []
+    fs.createReadStream("CSV/school_book.csv")
+      .pipe(parse({ delimiter: ",", from_line: 2 }))
+      .on("data", function (row) {
+        arr.push(row);
+    })
+    .on("end", function () {
+        res.send(arr);
+    })
+    .on("error", function (error) {
+      console.log(error.message);
     });
 });
-
-app.post('/read', upload.single('file'), (req, res) => {
-    const file = req.file
-  
-    const data = fs.readFileSync(file.path)
-    parse(data, (err, records) => {
-      if (err) {
-        console.error(err)
-        return res.status(400).json({success: false, message: 'An error occurred'})
-      }
-  
-      return res.json({data: records})
-    })
-})
 
 app.listen(port, () => {
   console.log(`App listening on port http://127.0.0.1:${port}`)
